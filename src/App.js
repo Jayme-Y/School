376239828bbb28e992d67c27f4478286a4b49f30
Jayme-Y <jaymeyereance@aol.com>
2025-04-12 02:50:54 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import logo from './logo.svg';
 import './App.css';
-import EmployeeForm from './Components/EmployeeForm';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Employee Form</h1> 
-        <EmployeeForm /> 
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
